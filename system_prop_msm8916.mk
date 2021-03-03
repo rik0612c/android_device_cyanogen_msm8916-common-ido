68342d7c16138954b2ad15259876af6a0ce73297
Wilson Sung <wilsonsung@google.com>
2021-03-03 18:14:54 +0800

    msm8916-common: Set SOC manufacturer and chipset properties
    Bug: 158284209
    Bug: 180589452
    Change-Id: I26d4b3767d336321bd562a8d5a289ce864536f5e

--- system_prop_msm8916.mk
+++ system_prop_msm8916.mk
@@ -5,3 +5,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.min_freq_0=800000
+
+# SoC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=MSM8916
