9b86096e7f5aff9e5204a78ebed52348706e5388
orges <me@orgesified.ml>
2021-10-26 21:25:34 +0800

    msm8916-common: config: Add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: I1716319bb17b9236d07d44a2a7fa3c7640c54ddf

--- msm8916.mk
+++ msm8916.mk
@@ -86,6 +86,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Do not spin up a separate process for the network stack, use an in-process APK.
 PRODUCT_PACKAGES += InProcessNetworkStack
 PRODUCT_PACKAGES += com.android.tethering.inprocess
