5831347013bdb3318d09b068efd421a256abf640
SamarV-121 <samarvispute121@pm.me>
2022-02-10 09:32:03 +0800

    msm8916-common: Remove obsolete debug.sf.disable_backpressure property
     * It has been removed in Android S
     * LineageOS/android_frameworks_native@2ec5391
    
    Change-Id: I23a210a37ad257f0c8b7a3c5e1d59b2ac20cde9a

--- system_prop.mk
+++ system_prop.mk
@@ -29,7 +29,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false \
     debug.mdpcomp.idletime=600 \
     debug.renderengine.backend=threaded \
-    debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     persist.hwc.mdpcomp.enable=true \
     persist.hwc.ptor.enable=true \
