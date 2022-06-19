1989d0b113da4b30e14b7cc2a01313de23a908ad
Ricky Cheung <rcheung844@gmail.com>
2022-06-19 14:33:59 +0800

    msm8916-common: Remove RenderScript HAL for good
    * Deprecated
    
    Change-Id: I17180440a6130704cfa9b37e7b1f4d645f3831ee

--- msm8916.mk
+++ msm8916.mk
@@ -221,10 +221,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_cm
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
