9b059de99ab8a224f8a8107bb7e93960873161cb
Ricky Cheung <rcheung844@gmail.com>
2022-06-19 15:47:20 +0800

    msm8916-common: Move Wi-Fi overlays to ServiceWifiResources for Android 12
    Change-Id: I995d291905c532f85269295237d1f73e33bece69

--- msm8916.mk
+++ msm8916.mk
@@ -19,6 +19,7 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/modules/Wifi/service/ServiceWifiResources
 
 # ANT+
 PRODUCT_PACKAGES += \
